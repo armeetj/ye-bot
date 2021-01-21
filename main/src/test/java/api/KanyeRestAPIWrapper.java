f797a2bb2084b397e7486276180f84820212614d
Armeet Jatyani <armeetjatyani@gmail.com>
2021-01-21 00:19:17 -0800

    Auto stash before rebase of "refs/heads/main"

---
+++ main/src/test/java/api/KanyeRestAPIWrapper.java
@@ -0,0 +1,16 @@
+// @author ArmeetJatyani
+// kanye.rest REST API Wrapper
+
+package api;
+
+public class KanyeRestAPIWrapper
+{
+    /**
+     * https://api.kanye.rest
+     * @return
+     */
+    public static String get()
+    {
+        return "random quote ;)";
+    }
+}
---
+++ main/src/test/java/utils/EmbedBuilder.java
@@ -0,0 +1,24 @@
+// @author ArmeetJatyani
+// quickly build Discord Embeds
+
+package utils;
+
+public class EmbedBuilder
+{
+    /**
+     * constructor
+     */
+    public EmbedBuilder()
+    {
+
+    }
+
+    /**
+     * build embed
+     * @return
+     */
+    public EmbedBuilder build()
+    {
+        return null;
+    }
+}
